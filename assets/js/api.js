6dace7a6093228f60fb9966b868cc4d9bab3f283
Sakuja Shamal <sakuja1shamal@gmail.com>
2023-04-28 08:51:26 +0530

    APIs updating 1.0

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,68 @@
-/**
- * @license MIT
- * @fileoverview All api related stuff like api_key, api request etc.
- * @copyright codewithsadee 2023 All rights reserved
- * @author codewithsadee <mohammadsadee24@gmail.com>
- */
+/*
+These free apis from https://openweather.co.uk/
+
+Email   - sakuja1shamal@gmail.com
+Key     - 31d01689c86421ea1062b631094e52ba
+Name    - Rainy.com
+Status  - Active
+Actions - On
+*/
+
+const api_key = "31d01689c86421ea1062b631094e52ba";
+
+export const fetchData = (URL, callback) => {
+    fetch(`${URL}&appid=${api_key}`)
+        .then(res => res.json())
+        .then(data => callback(data));
+}
+
+/* APIs */
+export const url = {
+    currentWeather: (lat,lon) => {
+        return `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric`;
+    },
+
+    fiveDayForecast:(lat,lon)=>{
+        return `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric`;
+    },
+
+    airPollution:(lat,lon)=>{
+        return `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}`;
+    },
+
+    reverseGeo:(lat,lon)=>{
+        return `https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=5`;
+    },
+
+    /* Query is a string type searched text, that may be city,state or country */
+    geo:(query)=>{
+      return `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
+    }
+};
+
+
+/*
+
+fetchData(url.currentWeather(6.792538,80.952419),data=>{
+    console.log(data);
+})
+
+fetchData(url.fiveDayForecast(6.792538,80.952419),data=> {
+    console.log(data);
+});
+
+fetchData(url.airPollution(6.792538,80.952419),data=>{
+    console.log(data);
+});
+
+fetchData(url.reverseGeo(6.792538,80.952419),data=>{
+    console.log(data);
+});
+
+fetchData(url.geo('london'),data=>{
+    console.log(data);
+})
+
+*/
+
+
